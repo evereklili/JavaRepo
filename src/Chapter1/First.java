6e42f3ae16e2917309c040a6872bbf7d86356ea0
OMER DOGU <omerdogu@omerdogu.com>
2022-12-14 02:31:30 +0300

    First Commite

---
+++ src/Chapter1/First.java
@@ -0,0 +1,15 @@
+package Chapter1;
+
+public class First {
+
+	public First() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First Java Application OMER DOGU ");
+	}
+
+}
